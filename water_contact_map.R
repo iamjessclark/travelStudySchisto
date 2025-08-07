f7d9e3c35ffe8ed3f032a241920f426724274126
Rivka1234 <rivkamlim@gmail.com>
2025-08-07 19:25:38 +0100

    maps

---
+++ water_contact_map.R
@@ -0,0 +1,4 @@
+# [Content removed during history cleanup: deprecated script that
+# contained a hardcoded Google Maps API key. Superseded by
+# Maps_open.R, which needs no API key and reads coordinates from a
+# gitignored private_data/ file.]
